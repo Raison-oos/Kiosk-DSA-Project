736210a9fc1800205468a583611ec163558f7c9b
Rai <raison177013@gmail.com>
2024-12-22 14:21:23 +0800

    no design

---
+++ Menu_Program/Logs.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Menu_Program
+{
+    partial class Logs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOrders = new System.Windows.Forms.Button();
+            this.btnTodayLogs = new System.Windows.Forms.Button();
+            this.btnLogs = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.lblTotalSales = new System.Windows.Forms.Label();
+            this.btnServe = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOrders
+            // 
+            this.btnOrders.Location = new System.Drawing.Point(60, 102);
+            this.btnOrders.Name = "btnOrders";
+            this.btnOrders.Size = new System.Drawing.Size(75, 23);
+            this.btnOrders.TabIndex = 0;
+            this.btnOrders.Text = "Orders";
+            this.btnOrders.UseVisualStyleBackColor = true;
+            this.btnOrders.Click += new System.EventHandler(this.btnOrders_Click);
+            // 
+            // btnTodayLogs
+            // 
+            this.btnTodayLogs.Location = new System.Drawing.Point(60, 144);
+            this.btnTodayLogs.Name = "btnTodayLogs";
+            this.btnTodayLogs.Size = new System.Drawing.Size(75, 23);
+            this.btnTodayLogs.TabIndex = 1;
+            this.btnTodayLogs.Text = "Today\'s Logs";
+            this.btnTodayLogs.UseVisualStyleBackColor = true;
+            this.btnTodayLogs.Click += new System.EventHandler(this.btnTodayLogs_Click);
+            // 
+            // btnLogs
+            // 
+            this.btnLogs.Location = new System.Drawing.Point(60, 185);
+            this.btnLogs.Name = "btnLogs";
+            this.btnLogs.Size = new System.Drawing.Size(75, 23);
+            this.btnLogs.TabIndex = 2;
+            this.btnLogs.Text = "Logs";
+            this.btnLogs.UseVisualStyleBackColor = true;
+            this.btnLogs.Click += new System.EventHandler(this.btnLogs_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(174, 0);
+            this.dataGrid.MultiSelect = false;
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGrid.Size = new System.Drawing.Size(833, 551);
+            this.dataGrid.TabIndex = 3;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(60, 492);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 4;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // lblTotalSales
+            // 
+            this.lblTotalSales.AutoSize = true;
+            this.lblTotalSales.Location = new System.Drawing.Point(12, 452);
+            this.lblTotalSales.Name = "lblTotalSales";
+            this.lblTotalSales.Size = new System.Drawing.Size(76, 13);
+            this.lblTotalSales.TabIndex = 5;
+            this.lblTotalSales.Text = "label im a label";
+            // 
+            // btnServe
+            // 
+            this.btnServe.Location = new System.Drawing.Point(60, 463);
+            this.btnServe.Name = "btnServe";
+            this.btnServe.Size = new System.Drawing.Size(75, 23);
+            this.btnServe.TabIndex = 6;
+            this.btnServe.Text = "Serve";
+            this.btnServe.UseVisualStyleBackColor = true;
+            this.btnServe.Visible = false;
+            this.btnServe.Click += new System.EventHandler(this.btnServe_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(47, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.comboBox1_SelectedValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Sort:";
+            // 
+            // Logs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 550);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnServe);
+            this.Controls.Add(this.lblTotalSales);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.btnLogs);
+            this.Controls.Add(this.btnTodayLogs);
+            this.Controls.Add(this.btnOrders);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Logs";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logs";
+            this.Load += new System.EventHandler(this.Logs_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOrders;
+        private System.Windows.Forms.Button btnTodayLogs;
+        private System.Windows.Forms.Button btnLogs;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblTotalSales;
+        private System.Windows.Forms.Button btnServe;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Menu_Program/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Menu_Program
+{
+    internal class Menu
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public string category { get; set; }
+        public Image image { get; set; }
+        //public string description { get; set; }
+
+        public Menu(string name, int price, string category, Image image)
+        {
+            this.name = name;
+            this.price = price;
+            this.category = category;
+            this.image = image;
+        }
+    }
+}
